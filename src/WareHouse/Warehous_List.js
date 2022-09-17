c0c0c9c3cd5cb68ad0f0fa9aea1c114494284ae9
KhakedSobhy22 <khaled.sobhy@easwaaqmisr.com>
2022-09-17 15:17:05 +0200

    fix stockcard modal  error

--- src/WareHouse/Warehous_List.js
+++ src/WareHouse/Warehous_List.js
@@ -1,74 +1,86 @@
-import Header from "../Header";
-import { Table } from 'react-bootstrap';
-import React,{ useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import Modal from 'react-bootstrap/Modal';
-
-import Button from 'react-bootstrap/Button';
+import { Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
+import Button from "react-bootstrap/Button";
 
 function ListProduct() {
-    const [data, setData] = useState([]);
-    const token = localStorage.getItem('token');
-    const userToken = JSON.parse(token);
-    const [show_item, setShow_item] = useState(true);
+  const [data, setData] = useState([]);
+  const token = localStorage.getItem("token");
+  const userToken = JSON.parse(token);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const config = {
-        headers: { Authorization: `Bearer ${userToken}` }
-    };
-    async function getData(){
-        let result = await fetch("http://stockapi.ghonaim.com/api/stock/warehouses/index", config);
-        result = await result.json();
-        console.log(result);
-        setData(result.data);
+  console.log("here");
+  const config = {
+    headers: { Authorization: `Bearer ${userToken}` },
+  };
+  async function getData() {
+    setIsLoading(true);
+    try {
+      let result = await fetch(
+        "http://stockapi.ghonaim.com/api/stock/warehouses/index",
+        config
+      );
+      result = await result.json();
+      console.log(result);
+      setIsLoading(false);
+      setData(result.data);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
     }
+  }
 
-    const handleClose = () => {
-        setShow_item(false)};
-    
-    useEffect(async ()=> {
-            getData()
-    }, [])
-//    async function showOper(id){
-//         let result = await fetch('http://stockapi.ghonaim.com/api/stock/warehouses/'id+,config );
-//         result = result.json();
-//         getData();
-//     }
-    return (
+  useEffect(() => {
+    getData();
+  }, []);
+  //    async function showOper(id){
+  //         let result = await fetch('http://stockapi.ghonaim.com/api/stock/warehouses/'id+,config );
+  //         result = result.json();
+  //         getData();
+  //     }
+  return (
+    <div className="row justify-content-center mt-4">
+      <Card className="m-2 p-2 col-lg-6 col-md-8 col-sm-10">
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>ACTIVE </th>
+                <th>DETAILS</th>
+              </tr>
+            </thead>
 
-        
-        <>
-         <Modal show={show_item} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Table >
-                        <tr>
-                            <th>id</th>
-                            <th>Name</th>
-                            <th>is_active</th>
-                            
-                        </tr>
-                    
-                        {
-                            data.map((item)=>
-                                <tr key={item.id}>
-                                    <td>{item.id}</td>
-                                    <td>{item.name}</td>
-                                    <td>{item.is_active}</td>
-                                    <td><Link to={`/warehouse/${item.id}`} > <Button className='m-1'>Show warehouse</Button></Link>
-                                     
-                                     </td>
-                                </tr>
-                            )
-                        }   
+            <tbody>
+              {isLoading && (
+                <Spinner
+                  animation="border"
+                  role="status"
+                  className="centerSpinner"
+                />
+              )}
+              {data.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.is_active ? "Yes" : "No"}</td>
+                  <td>
+                    <Link to={`/warehouse/${item.id}`}>
+                      {" "}
+                      <Button className="m-1">Show warehouse</Button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
 
-                </Table>
-        </Modal.Body>
-      </Modal>
-
-            
-                {/* <h1>product list site</h1>
+      {/* <h1>product list site</h1>
                 <div className='col-sm-8 offset-sm-2 '>
                 <Table >
                         <tr>
@@ -93,8 +105,7 @@ function ListProduct() {
 
                 </Table>
                 </div> */}
-        </>
-    )
-
+    </div>
+  );
 }
-export default ListProduct
+export default ListProduct;
