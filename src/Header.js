2358991c4c0064c0883134740bbcfdca2a15a1f7
KhakedSobhy22 <khaled.sobhy@easwaaqmisr.com>
2022-09-17 12:12:53 +0200

    fix logout button style

--- src/Header.js
+++ src/Header.js
@@ -1,55 +1,52 @@
-import { useContext, useState, useSyncExternalStore } from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { Link,useHistory } from 'react-router-dom';
-import {UserContext} from './user/userContext';
-import './header.css';
+import { useContext, useState, useSyncExternalStore } from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { UserContext } from "./user/userContext";
+import "./header.css";
 
 function Header() {
-    const history = useHistory('');
-	const tokenContext = useContext(UserContext);
-	console.log("999", tokenContext);
+  const history = useHistory("");
+  const tokenContext = useContext(UserContext);
+  console.log("999", tokenContext);
 
-    function logOut(){
-        localStorage.clear();
-        history.push('/login');
-		tokenContext.setLogged(false);
-    }
-    let user = JSON.parse(localStorage.getItem('token'))
-    return (
-        <div className='header'>
-            <Navbar>
-
-                <a href="/" className='logo_text'>STOCK MANAGEMENT</a>
-                <Nav className="me-auto navbar_wrapper links">
-                    {
-                        tokenContext.token ?
-                            <>
-                                {/* <Link to="/add">add product</Link>
+  function logOut() {
+    localStorage.clear();
+    history.push("/login");
+    tokenContext.setLogged(false);
+  }
+  let user = JSON.parse(localStorage.getItem("token"));
+  return (
+    <div className="header">
+      <Navbar>
+        <a href="/" className="logo_text">
+          STOCK MANAGEMENT
+        </a>
+        <Nav className="me-auto navbar_wrapper links">
+          {tokenContext.token ? (
+            <>
+              {/* <Link to="/add">add product</Link>
                                 <Link to="/update">update product</Link> */}
-                                <Link to="/masterfile">MasterFile</Link>
-                                <Link to="/list">StockCard</Link>
-                                <Link to="/postReceiving">Receiving</Link>
-
-                            </>
-                             :
-                            <>
-                                <Link to="/login">login</Link>
-                            </>
-                    }
-
-                </Nav>
-                {
-                    localStorage.getItem('token')?
-                <Nav style={{marginRight:'120px'}} >
-                    <NavDropdown title={user && user.name}>
-                        <NavDropdown.Item onClick={logOut} >logout</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>:null
-                }
-
-
-            </Navbar>
-        </div>
-    )
+              <Link to="/masterfile">MasterFile</Link>
+              <Link to="/list">StockCard</Link>
+              <Link to="/postReceiving">Receiving</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login">login</Link>
+            </>
+          )}
+        </Nav>
+        {localStorage.getItem("token") ? (
+          <Nav style={{ marginRight: "120px" }}>
+            <NavDropdown title={user && user.name}>
+              <NavDropdown.Item onClick={logOut} className="text-primary">
+                logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        ) : null}
+      </Navbar>
+    </div>
+  );
 }
-export default Header
+export default Header;
